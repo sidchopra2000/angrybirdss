a8cc0e99c354a0f516d1b69dcc0e6c83a6f84fce
sidchopra2000 <64650949+sidchopra2000@users.noreply.github.com>
2020-05-28 18:41:47 +0530

    Add files via upload
    ..

---
+++ chain.js
@@ -0,0 +1,17 @@
+class ChainClass{
+    constructor(bodyA,bodyB) {
+        var options = {
+            'bodyA':bodyA,
+            'bodyB':bodyB,
+            'stiffness':0.3,
+            'length':10,
+        }
+       this.chain=Constraint.create(options)
+        World.add(world, this.chain);
+      }
+      display(){
+          strokeWeight(4)
+        line(this.chain.bodyA.position.x,this.chain.bodyA.position.y,this.chain.bodyB.position.x,this.chain.bodyB.position.y)
+
+      }
+}
